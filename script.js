25b3d5eefd6abe19acaf15e3532bf5735a172e84
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-11-25 06:24:31 +0100

    define variables from DOM

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements from DOM and define them in variables
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('.current--0');
+const current1El = document.getElementById('.current--1');
+const NewGameBtn = document.querySelector('.btn--new');
+const RollDiceBtn = document.querySelector('.btn--roll');
+const holdScoreBtn = document.querySelector('.btn--hold');
